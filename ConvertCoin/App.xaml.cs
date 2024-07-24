02d95803f1b1b474b1df04c40f153e8b4fa07703
Parampreet30 <A00264414@mycambrian.ca>
2024-07-24 16:13:02 -0400

    Add project files.

---
+++ ConvertCoin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ConvertCoin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ConvertCoin/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ConvertCoin
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
