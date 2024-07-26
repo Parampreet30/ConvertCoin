0613152696b75be3c2bc3b3d12916262814eedf2
Parampreet30 <A00264414@mycambrian.ca>
2024-07-26 12:51:52 -0400

    HomeViewModel created

---
+++ ConvertCoin/ViewModel/HomeViewModel.cs
@@ -0,0 +1,40 @@
+﻿
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ConvertCoin.ViewModel
+{
+    public partial class HomeViewModel : ObservableObject
+    {
+        /*
+         amount: this will have the currency of the country that is to be converted
+         convertedAmount: this will be the converted currency of the desired country
+         amountSelectedCountry: this will have the country code of the country that is to be converted
+        convertedAmountCountry: this will be the desired country for which the currency is being converted  
+        
+         */
+
+        [ObservableProperty]
+        double amount;
+        [ObservableProperty]
+        double convertedAmount;
+        [ObservableProperty]
+        string amountSelectedCountry;
+        [ObservableProperty]
+        string convertedAmountCountry;
+
+        [RelayCommand]
+        void SwitchCountries()
+        {
+
+        }
+        [RelayCommand]
+        void Country() { }
+
+        [RelayCommand]
+        void CovertedCountries() { }
+
+
+    }
+}
