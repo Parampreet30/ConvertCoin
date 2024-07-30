baae0f752dd26bfb5baa2b545931e7e9101b89aa
baljinder <baljindermaan15@gmail.com>
2024-07-30 11:07:34 -0400

    comments added

--- ConvertCoin/ViewModel/LoginViewModel.cs
+++ ConvertCoin/ViewModel/LoginViewModel.cs
@@ -3,14 +3,24 @@ using CommunityToolkit.Mvvm.Input;
 
 namespace  ConvertCoin.ViewModel;
 
+/*
+
+*/
+
 public partial class LoginViewModel : ObservableObject
 {
+    // Stores the username entered by the user
     [ObservableProperty]
     string username;
 
-
+// Stores the password entered by the user
     [ObservableProperty]
     string password;
+
+    // Command to be executed when the login button is clicked
+        public IAsyncRelayCommand LoginCommand { get; }
+
+    
 
     [RelayCommand]
     void Login(){
